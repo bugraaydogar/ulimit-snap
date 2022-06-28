e66fab3fbf886880f4178e82e387f18a722f18b0
Buğra Aydoğar <bugra.aydogar@canonical.com>
2022-06-28 15:47:46 +0300

    Update typo

--- bin/increase-ulimit.sh
+++ bin/increase-ulimit.sh
@@ -4,7 +4,7 @@ echo "Increasing the ulimit"
 files=$(ulimit -n)
 echo "Current ulimit value $files"
 
-echo "Increasing ulimit value to 8192"
+echo "Increasing ulimit value to 16384"
 ulimit -n 16384
 
 echo "Increasing the ulimit done"
